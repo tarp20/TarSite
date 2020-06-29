527461203943612502c86ea3f2b64d63a6334a20
Taras Piont <taras.piont@gmail.com>
2020-06-29 11:27:07 +0200

    Update models.py

--- news/models.py
+++ news/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class News(models.Model):
     
+
     title = models.CharField(max_length=150)
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
