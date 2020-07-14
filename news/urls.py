f32afa7dc4c152b9b142f66843b247f077136b44
Taras Piont <taras.piont@gmail.com>
2020-07-14 13:56:11 +0200

    som

--- news/urls.py
+++ news/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
-from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('', index),
-    path('test/',test),
-
-
+    path('admin/', admin.site.urls),
+    path('', include('news.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
